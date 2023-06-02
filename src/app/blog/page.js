335037452f079c11676cea77ffd093387f072349
Jakub Wojtysiak <it.jakub.wojtysiak@gmail.com>
2023-06-02 17:39:58 +0200

    Added blog and article tiles

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,23 +1,81 @@
+import styles from "@/styles/blog.module.scss";
+import Navigation from "@/components/Header";
+import MobileNav from "@/components/MobileNav";
+import Footer from "@/components/Footer";
+import ArticleTile from "@/components/ArticleTile";
+import C1 from "@/images/galery/comparission1.webp";
+import Image from "next/image";
 
-
-export default function BlogPage() {
-    return (
-      <div className={`pagesBackground ${styles.mobile}`}>
-        <header>
-          <Navigation type="top" page="normal" />
-          <MobileNav page="page" />
-        </header>
-        <main className={styles.main}>
-          <div className="container">
-            <h1>Blog</h1>
-            <div>
-                
-            </div>
+export default function Blog() {
+  return (
+    <div className={`pagesBackground ${styles.mobile}`}>
+      <header>
+        <Navigation type="top" page="normal" />
+        <MobileNav page="page" />
+      </header>
+      <main className={styles.main}>
+        <div className="container">
+          <h1>Blog</h1>
+          <div className={styles.articlesContainer}>
+            <ArticleTile
+              title="Pierwszy Post Na Naszym Blogu"
+              description="Profesjonalna pielęgnacja trawnika nie jest niczym ciężkim. Do spełnienia marzeń o posiadaniu idealnej trawy nie potrzeba niczego więcej jak odrobiny cierpliwości..."
+              subject="Pielęgnacja trawnika"
+              author="Jan Kowalski"
+              date="2021-01-01"
+              link="/blog/1"
+              image={C1}
+            />
+            <ArticleTile
+              title="Pierwszy Post Na Naszym Blogu"
+              description="Profesjonalna pielęgnacja trawnika nie jest niczym ciężkim. Do spełnienia marzeń o posiadaniu idealnej trawy nie potrzeba niczego więcej jak odrobiny cierpliwości..."
+              subject="Pielęgnacja trawnika"
+              author="Jan Kowalski"
+              date="2021-01-01"
+              link="/blog/1"
+              image={C1}
+            />
+            <ArticleTile
+              title="Pierwszy Post Na Naszym Blogu"
+              description="Profesjonalna pielęgnacja trawnika nie jest niczym ciężkim. Do spełnienia marzeń o posiadaniu idealnej trawy nie potrzeba niczego więcej jak odrobiny cierpliwości..."
+              subject="Pielęgnacja trawnika"
+              author="Jan Kowalski"
+              date="2021-01-01"
+              link="/blog/1"
+              image={C1}
+            />
+            <ArticleTile
+              title="Pierwszy Post Na Naszym Blogu"
+              description="Profesjonalna pielęgnacja trawnika nie jest niczym ciężkim. Do spełnienia marzeń o posiadaniu idealnej trawy nie potrzeba niczego więcej jak odrobiny cierpliwości..."
+              subject="Pielęgnacja trawnika"
+              author="Jan Kowalski"
+              date="2021-01-01"
+              link="/blog/1"
+              image={C1}
+            />
+            <ArticleTile
+              title="Pierwszy Post Na Naszym Blogu"
+              description="Profesjonalna pielęgnacja trawnika nie jest niczym ciężkim. Do spełnienia marzeń o posiadaniu idealnej trawy nie potrzeba niczego więcej jak odrobiny cierpliwości..."
+              subject="Pielęgnacja trawnika"
+              author="Jan Kowalski"
+              date="2021-01-01"
+              link="/blog/1"
+              image={C1}
+            />
+            <ArticleTile
+              title="Pierwszy Post Na Naszym Blogu"
+              description="Profesjonalna pielęgnacja trawnika nie jest niczym ciężkim. Do spełnienia marzeń o posiadaniu idealnej trawy nie potrzeba niczego więcej jak odrobiny cierpliwości..."
+              subject="Pielęgnacja trawnika"
+              author="Jan Kowalski"
+              date="2021-01-01"
+              link="/blog/1"
+              image={C1}
+            />
           </div>
-        </main>
-  
-        <Footer />
-      </div>
-    );
-  }
-  
+        </div>
+      </main>
+
+      <Footer />
+    </div>
+  );
+}
---
+++ src/components/ArticleTile.js
@@ -0,0 +1,45 @@
+import styles from "@/styles/articleTile.module.scss";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function ArticleTile({
+  title,
+  description,
+  subject,
+  author,
+  date,
+  link,
+  image,
+}) {
+  return (
+    <article className={styles.articleTile}>
+      <div className={styles.imageWrapper}>
+        <Image src={image} />
+        <div className={styles.shadow}></div>
+      </div>
+      <div className={styles.articleDescription}>
+        <h2>{title}</h2>
+        <p>{description}</p>
+        <div className={styles.articleFooter}>
+          <div className={styles.details}>
+            <p className={styles.cyan}>
+              Tematyka: <span>{subject}</span>
+            </p>
+            <p className={styles.cyan}>
+              Autor: <span>{author}</span>
+            </p>
+            <p className={styles.cyan}>
+              Opublikowany:{" "}
+              <span>
+                <date>{date}</date>
+              </span>
+            </p>
+          </div>
+          <button className={styles.moreInfo}>
+            <Link href={link}>Czytaj więcej...</Link>
+          </button>
+        </div>
+      </div>
+    </article>
+  );
+}
